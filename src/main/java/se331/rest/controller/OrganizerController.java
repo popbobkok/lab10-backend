6c83974a1cce7c4c81b69d94f4814754cdb667d9
Natthapong_Pop <popbobkok34@hotmail.com>
2022-09-22 18:07:48 +0700

    6.1 Organizer Management

---
+++ src/main/java/se331/rest/controller/OrganizerController.java
@@ -0,0 +1,18 @@
+package se331.rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se331.rest.service.OrganizerService;
+
+@RestController
+public class OrganizerController {
+
+    OrganizerService organizerService;
+
+    @GetMapping("/organizers")
+    ResponseEntity<?> getOrganizers(){
+        return ResponseEntity.ok(organizerService.getAllOrganizer());
+    }
+}
---
+++ src/main/java/se331/rest/service/OrganizerServicelmpl.java
@@ -0,0 +1,24 @@
+package se331.rest.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import se331.rest.dao.OrganizerDao;
+import se331.rest.entity.Organizer;
+
+import java.util.List;
+
+public class OrganizerServicelmpl implements OrganizerService{
+    @Autowired
+    OrganizerDao organizerDao;
+    @Override
+    public List<Organizer> getAllOrganizer(){
+        return organizerDao.getOrganizer(Pageable.unpaged()).getContent();
+    }
+
+    @Override
+    public Page<Organizer> getOrganizer(Integer page, Integer pageSize){
+        return organizerDao.getOrganizer(PageRequest.of(page,pageSize));
+    }
+}
