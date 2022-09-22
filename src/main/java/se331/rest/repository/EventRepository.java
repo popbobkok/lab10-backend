1a5c5d1bc5d0348d161162a75b59f010ae184d39
Natthapong_Pop <popbobkok34@hotmail.com>
2022-09-22 13:14:43 +0700

    2.2 (can't run yet)

--- src/main/java/se331/rest/repository/EventRepository.java
+++ src/main/java/se331/rest/repository/EventRepository.java
@@ -3,9 +3,11 @@ package se331.rest.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import se331.rest.entity.Event;
+import se331.rest.entity.Organizer;
 
 import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event,Long> {
     List<Event> findAll();
 }
+
---
+++ src/main/java/se331/rest/repository/OrganizerRepository.java
@@ -0,0 +1,11 @@
+package se331.rest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se331.rest.entity.Event;
+
+import java.util.List;
+
+public interface OrganizerRepository extends JpaRepository<Event,Long> {
+    //List<Event> findAll();
+}
+
