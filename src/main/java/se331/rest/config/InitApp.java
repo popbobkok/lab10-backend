b33871a834e12db47dfe65e8d9771dda271d3cdb
Natthapong_Pop <popbobkok34@hotmail.com>
2022-09-22 14:10:01 +0700

    2.4 not work yet

--- src/main/java/se331/rest/config/InitApp.java
+++ src/main/java/se331/rest/config/InitApp.java
@@ -5,16 +5,27 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import se331.rest.entity.Event;
+import se331.rest.entity.Organizer;
 import se331.rest.repository.EventRepository;
+import se331.rest.repository.OrganizerRepository;
 
 @Component
 public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
     @Autowired
     EventRepository eventRepository;
 
+    @Autowired
+    OrganizerRepository organizerRepository;
+
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
-        eventRepository.save(Event.builder()
+        //eventRepository.save(Event.builder()
+        Organizer org1,org2,org3;
+        org1 = organizerRepository.save(Organizer.builder().name("CAMT").build());
+        org2 = organizerRepository.save(Organizer.builder().name("CMU").build());
+        org3 = organizerRepository.save(Organizer.builder().name("Chaingmai").build());
+        Event tempEvent;
+        tempEvent = eventRepository.save(Event.builder()
                 .category("Academic")
                 .title("Midterm Exam")
                 .description("A time for taking the exam")
@@ -23,7 +34,9 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .time("3.00-4.00 pm.")
                 .petAllowed(false)
                 .build());
-        eventRepository.save(Event.builder()
+        tempEvent.setOrganizer(org1);
+        org1.getOwnEvents().add(tempEvent);
+        tempEvent = eventRepository.save(Event.builder()
                 .category("Academic")
                 .title("Commencement Day")
                 .description("A time for celebration")
@@ -32,7 +45,9 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .time("8.00am-4.00 pm.")
                 .petAllowed(false)
                 .build());
-        eventRepository.save(Event.builder()
+        tempEvent.setOrganizer(org1);
+        org1.getOwnEvents().add(tempEvent);
+        tempEvent = eventRepository.save(Event.builder()
                 .category("Cultural")
                 .title("Loy Krathong")
                 .description("A time for Krathong")
@@ -41,7 +56,9 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .time("8.00-10.00 pm.")
                 .petAllowed(false)
                 .build());
-        eventRepository.save(Event.builder()
+        tempEvent.setOrganizer(org2);
+        org2.getOwnEvents().add(tempEvent);
+        tempEvent = eventRepository.save(Event.builder()
                 .category("Cultural")
                 .title("Songkran")
                 .description("Let's Play Water")
@@ -50,5 +67,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .time("10.00am - 6.00 pm.")
                 .petAllowed(true)
                 .build());
+        tempEvent.setOrganizer(org3);
+        org3.getOwnEvents().add(tempEvent);
     }
 }
--- src/main/java/se331/rest/repository/OrganizerRepository.java
+++ src/main/java/se331/rest/repository/OrganizerRepository.java
@@ -2,10 +2,11 @@ package se331.rest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import se331.rest.entity.Event;
+import se331.rest.entity.Organizer;
 
 import java.util.List;
 
-public interface OrganizerRepository extends JpaRepository<Event,Long> {
+public interface OrganizerRepository extends JpaRepository<Organizer,Long> {
     //List<Event> findAll();
 }
 
